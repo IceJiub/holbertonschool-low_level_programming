cfee9b0a60008e0ba3c7d2ea274afe7dc09fff05
IceJiub <tom.yang@hotmail.fr>
2024-06-17 09:08:41 +0000

    Update 4-puts: betty coding style

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
-    char* str1 = "\"Programming is like building a multilingual puzzle"; 
+    char *str1 = "\"Programming is like building a multilingual puzzle"; 
     puts(str1);
- 
-    return 0;
 }
