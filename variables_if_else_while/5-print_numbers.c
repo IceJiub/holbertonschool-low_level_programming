0e50cc88470e4a9ee872cc67219c16af56d8750a
IceJiub <tom.yang@hotmail.fr>
2024-06-17 14:16:25 +0000

    Add 5-print_numbers.c

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Prints the alphabet in lowercase
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
