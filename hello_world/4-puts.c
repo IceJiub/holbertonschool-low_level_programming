5a0aa76640e4edff5176d3949f85d1ae64d00136
IceJiub <tom.yang@hotmail.fr>
2024-06-17 08:50:51 +0000

    Update 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 int main()
 {
-    char str1 = "";
+    char str1 = "\"Programming is like building a multilingual puzzle\n"; 
     puts(str1);
- 
-    puts("\"Programming is like building a multilingual puzzle\n");
  
     return 0;
 }
