6930269f1506a5977dcc4b3216c55c91a889abb0
IceJiub <tom.yang@hotmail.fr>
2024-06-17 09:44:16 +0000

    Update 4-puts.c: add description for function main

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - Prints exactly "Programming is like building a multilingual puzzle 
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char *str1 = "\"Programming is like building a multilingual puzzle";
