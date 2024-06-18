be24ed5e3690ea050b75de9d62b2cde2ea61e8d4
IceJiub <tom.yang@hotmail.fr>
2024-06-18 13:48:13 +0000

    Add 4-isalpha and update main.h

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _isalpha - Check if the character is a letter, lowercase
+ * or uppercase
+ *
+ * @c: The character to check
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase
+ * 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else if (c >= 65 && c <= 90)
+		return (1);
+	else
+		return (0);
+}
