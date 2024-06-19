9f1f9ab96186c822a39ad398334283dd807bb579
IceJiub <tom.yang@hotmail.fr>
2024-06-19 13:51:45 +0000

    Add 8-24_hours.c and update main.h

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * jack_bauer - Print every minute of the day of Jack Bauer,
+ * start from 00:00 to 23:59
+ * Return: Always 0.
+ */
+void jack_bauer(void)
+{
+	int i = 0;
+	int h1 = 0;
+	int h2 = 0;
+	int m1 = 0;
+	int m2 = 0;
+
+	while (i < 1440 && m2 <= 9) {
+		_putchar(h1 + '0');
+		_putchar(h2 + '0');
+		_putchar(':');
+		_putchar(m1 + '0');
+		_putchar(m2 + '0');
+		_putchar('\n');
+		m2++;
+		if (m2 == 10)
+		{
+			m2 = 0;
+			m1++;
+		}
+		if (m1 == 6)
+		{
+			m1 = 0;
+			h2++;
+		}
+		if (h2 == 10)
+		{
+			h2 = 0;
+			h1++;
+		}
+	i++;
+	}
+}
