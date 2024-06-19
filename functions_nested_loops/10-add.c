78d8aa8b72c78643517c3848c2692ada5b2e4709
IceJiub <tom.yang@hotmail.fr>
2024-06-19 15:08:09 +0000

    Add 10-add.c and update main.h

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Add two integers and return the result.
+ * @a: First value
+ * @b: Second value
+ * Return: Result of the addition of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
