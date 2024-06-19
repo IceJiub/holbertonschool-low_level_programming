ed865d1ce3dd3b7479f9c7246b4bdb6dd27a8367
IceJiub <tom.yang@hotmail.fr>
2024-06-19 11:16:12 +0000

    Update 7-print_last_digit.c

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -11,17 +11,12 @@
  */
 int print_last_digit(int n)
 {
-	if (n == 0)
-	{
-		return (n);
-	}
-	else if (n < 0)
-	{
-		n = (n * -1) % 10;
-		return (n);
-	}
+	int last_digit;
+
+	if (n < 0)
+		last_digit = (n * -1) % 10;
 	else
-	{
-		return (n % 10);
-	}
+		last_digit = n % 10;
+	_putchar('0' + last_digit);
+	return (last_digit);
 }
