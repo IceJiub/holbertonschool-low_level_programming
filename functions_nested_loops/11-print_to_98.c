054ffa76ef816d71f4845dce89cba9790f38442c
IceJiub <tom.yang@hotmail.fr>
2024-06-20 12:08:15 +0000

    Update 11-print_to_98.c, refactoring code and create a function int_to_char to print a single number

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,5 +1,39 @@
 #include "main.h"
 /**
+ * int_to_char - Use _putchar to print int
+ * @i: number to print
+ */
+void int_to_char(int i)
+{
+	if (i < 0)
+	{
+		_putchar('-');
+		if (i <= -100)
+			_putchar('0' + i * -1 / 100);
+		if (i <= -10)
+		{
+			_putchar('0' + i * -1 / 10 % 10);
+			_putchar('0' + i * -1 % 10);
+		}
+		else
+			_putchar('0' + i * -1);
+	}
+	else
+	{
+		if (i >= 100)
+			_putchar('0' + i / 100);
+		if (i >= 10)
+		{
+			_putchar('0' + i / 10 % 10);
+			_putchar('0' + i % 10);
+		}
+		else
+			_putchar('0' + i);
+	}
+}
+
+
+/**
  * print_to_98 - Print all natural numbers from n to 98
  * @n: starting number to print following numbers from
  */
@@ -11,26 +45,7 @@ void print_to_98(int n)
 	{
 		for (i = n ; i <= 98 ; i++)
 		{
-			if (i < 0)
-			{
-				_putchar('-');
-				if (i <= -100)
-					_putchar('0' + i * -1 / 100);
-				if (i <= -10)
-				{
-					_putchar('0' + i * -1 / 10 % 10);
-					_putchar('0' + i * -1 % 10);
-				}
-				else
-					_putchar('0' + i * -1);
-			}
-			else if (i >= 10)
-			{
-				_putchar('0' + i / 10);
-				_putchar('0' + i % 10);
-			}
-			else if (i < 10)
-				_putchar('0' + i);
+			int_to_char(i);
 			if (i < 98)
 			{
 				_putchar(',');
