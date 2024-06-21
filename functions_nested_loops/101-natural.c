1da54f4ab6e15ced54f4d39c8d6c11f455736183
IceJiub <tom.yang@hotmail.fr>
2024-06-21 09:27:31 +0000

    Add 101-natural.c

---
+++ functions_nested_loops/101-natural.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ */
+void main(void)
+{
+	int n = 1024;
+	int resultat = 0;
+	int i, multiple_3, multiple_5;
+
+	multiple_3 = n / 3;
+	multiple_5 = n / 5;
+	
+	for (i = 1 ; i <= multiple_3 ; i++)
+	{
+		resultat += i * 3;
+	}
+	for (i = 1 ; i <= multiple_5 ; i++)
+	{
+		resultat += i * 5;
+	}
+
+	printf("%d\n", resultat);
+}
