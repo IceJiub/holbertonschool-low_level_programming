182c63ac22ca36f4920af63766367ea686d09bb6
IceJiub <tom.yang@hotmail.fr>
2024-06-24 09:18:21 +0000

    Update 5-more_numbers.c: replace spaces with indents

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -5,31 +5,31 @@
  */
 void put_int(int i)
 {
-        if (i < 0)
-        {
-                _putchar('-');
-                if (i <= -100)
-                        _putchar('0' + i * -1 / 100);
-                if (i <= -10)
-                {
-                        _putchar('0' + i * -1 / 10 % 10);
-                        _putchar('0' + i * -1 % 10);
-                }
-                else
-                        _putchar('0' + i * -1);
-        }
-        else
-        {
-                if (i >= 100)
-                        _putchar('0' + i / 100);
-                if (i >= 10)
-                {
-                        _putchar('0' + i / 10 % 10);
-                        _putchar('0' + i % 10);
-                }
-                else
-                        _putchar('0' + i);
-        }
+	if (i < 0)
+	{
+		_putchar('-');
+		if (i <= -100)
+			_putchar('0' + i * -1 / 100);
+		if (i <= -10)
+		{
+			_putchar('0' + i * -1 / 10 % 10);
+			_putchar('0' + i * -1 % 10);
+		}
+		else
+			_putchar('0' + i * -1);
+	}
+	else
+	{
+		if (i >= 100)
+			_putchar('0' + i / 100);
+		if (i >= 10)
+		{
+			_putchar('0' + i / 10 % 10);
+			_putchar('0' + i % 10);
+		}
+		else
+			_putchar('0' + i);
+	}
 }
 
 /**
@@ -39,6 +39,7 @@ void put_int(int i)
 void more_numbers(void)
 {
 	int line, n = 0;
+
 	for (line = 0 ; line < 10 ; line++)
 	{
 		for (n = 0 ; n <= 14 ; n++)
