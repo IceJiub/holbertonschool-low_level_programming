e77ade3a0c01326bc90d081a695c9ec268754ba9
IceJiub <tom.yang@hotmail.fr>
2024-06-24 09:04:13 +0000

    Add 5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - Print numbers from 0 to 14 ten times.
+ */
+
+void more_numbers()
+{
+	int line, n = 0;
+	for (line = 0 ; line < 10 ; line++)
+	{
+		for (n = 0 ; n <= 14 ; n++)
+		{
+			put_int(n);
+		}
+		_putchar('\n');
+	}
+}
