03c250a78d0400fd3bc9d99b4d948237b4d79428
IceJiub <tom.yang@hotmail.fr>
2024-06-24 09:13:35 +0000

    Update 5-more_numbers.c: add put_int function

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,5 +1,38 @@
 #include "main.h"
 /**
+ * put_int - Display number i using _putchar function
+ * @i: number to display
+ */
+void put_int(int i)
+{
+        if (i < 0)
+        {
+                _putchar('-');
+                if (i <= -100)
+                        _putchar('0' + i * -1 / 100);
+                if (i <= -10)
+                {
+                        _putchar('0' + i * -1 / 10 % 10);
+                        _putchar('0' + i * -1 % 10);
+                }
+                else
+                        _putchar('0' + i * -1);
+        }
+        else
+        {
+                if (i >= 100)
+                        _putchar('0' + i / 100);
+                if (i >= 10)
+                {
+                        _putchar('0' + i / 10 % 10);
+                        _putchar('0' + i % 10);
+                }
+                else
+                        _putchar('0' + i);
+        }
+}
+
+/**
  * more_numbers - Print numbers from 0 to 14 ten times.
  */
 
