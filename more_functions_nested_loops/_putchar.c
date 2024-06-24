585fe1b884f148698bb07874edfe221d1aeff57f
IceJiub <tom.yang@hotmail.fr>
2024-06-24 09:12:50 +0000

    Update _putchar.c: remove put_int function

--- more_functions_nested_loops/_putchar.c
+++ more_functions_nested_loops/_putchar.c
@@ -11,36 +11,3 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
-
-/**
- * put_int - Display number i using _putchar function
- * @i: number to display
- */
-void put_int(int i)
-{
-        if (i < 0)
-        {
-                _putchar('-');
-                if (i <= -100)
-                        _putchar('0' + i * -1 / 100);
-                if (i <= -10)
-                {
-                        _putchar('0' + i * -1 / 10 % 10);
-                        _putchar('0' + i * -1 % 10);
-                }
-                else
-                        _putchar('0' + i * -1);
-        }
-        else
-        {
-                if (i >= 100)
-                        _putchar('0' + i / 100);
-                if (i >= 10)
-                {
-                        _putchar('0' + i / 10 % 10);
-                        _putchar('0' + i % 10);
-                }
-                else
-                        _putchar('0' + i);
-        }
-}
