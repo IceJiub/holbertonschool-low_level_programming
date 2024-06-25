98a2dc9efb205ec188d2be95dadf7d160ba10c03
IceJiub <tom.yang@hotmail.fr>
2024-06-25 09:46:52 +0000

    Update 0-reset_to_98.c: modify description

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 /**
  * reset_to_98 - Take a pointer to an int as parameter
  * and update the value it points to 98.
- * @*n: pointer to update
+ * @n: pointer to update
  */
 
 void reset_to_98(int *n)
