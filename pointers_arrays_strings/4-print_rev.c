f2ca6192ae7679ae39180b5c1eba37a3a34935a8
IceJiub <tom.yang@hotmail.fr>
2024-06-25 14:56:34 +0000

    Update 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * print_rev - Print a string in reverse, followed by a new line
@@ -14,8 +13,8 @@ void print_rev(char *s)
 		i++;
 	while (i >= 0)
 	{
-		_putchar(s[i]);
 		i--;
+		_putchar(s[i]);
 	}
 	_putchar('\n');
 }
